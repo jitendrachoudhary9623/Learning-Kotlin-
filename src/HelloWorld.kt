55613b5797e5df6f1d4b2f622c7a2e44a3c2b326
Choudhary <choudhaj@pun-choudhaj-m.ds.ad.adp.com>
2019-10-19 10:25:40 +0530

    Wrote Hello World Program

---
+++ src/HelloWorld.kt
@@ -0,0 +1,12 @@
+//No Need to enclose code in a class like java
+//need a function main
+
+//similar to public staic void main(String[] args)
+//variablaname :datatype
+//Return type is after  fun main():Return type
+//Unit->Void
+//Integer -.Int
+fun main(args:Array<String>){
+    print("Hello World");
+    //println()
+}
