060e097c3f6e8dd64bdf57e509a008e135283263
Choudhary <choudhaj@pun-choudhaj-m.ds.ad.adp.com>
2019-10-19 10:33:35 +0530

    How hello world pgm works

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -7,6 +7,15 @@
 //Unit->Void
 //Integer -.Int
 fun main(args:Array<String>){
-    print("Hello World");
-    //println()
+    println("Hello World");
+    println(1+6)
+    println(10.0/9)
+   //println()
 }
+
+//kotlin needs JVM in runtime
+//kotlin creates class file internally
+//in run time code behaves similar to java code
+//kotlin compiler does this tasks
+//no static in kotlin
+//by default its static
